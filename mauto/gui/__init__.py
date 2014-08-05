e870e208dd30ce1eb5e1bc6463fa946b6f543797
csaez <cesarte@gmail.com>
2014-08-05 00:28:26 -0400

    gui redesign (not fully functional yet)

--- mauto/gui/__init__.py
+++ mauto/gui/__init__.py
@@ -1,38 +1,27 @@
 import os
 import sys
-import time
 import pkg_resources
 
 from mauto.api.lib import library
 
 try:
-    from maya import OpenMayaUI
+    from mauto.gui.mainwindow import Ui_MainWindow
     from PySide import QtGui, QtCore
     from shiboken import wrapInstance
-    from mauto.gui.main import Ui_MainWindow
-    from mauto.gui.inputs import Ui_Dialog
 except ImportError:
-    # stabs
     class QtGui():
-        QMainWindow = QDialog = object
+        QMainWindow = object
+try:
+    from maya import OpenMayaUI
+except ImportError:
+    pass
 
 
 class Layout(QtGui.QMainWindow):
-    # MODES[0]:Recorder, MODES[1]:Library
-    MODES = ({"list": True, "button": True, "filter": False,
-              "actionClipboard": True, "actionRecord": True,
-              "actionPause": True, "actionSave": True,
-              "actionShowLibrary": True, "actionPlay": False,
-              "actionRemove": False, "actionShowRecorder": False, },
-             {"list": True, "button": False, "filter": True,
-              "actionClipboard": False, "actionRecord": False,
-              "actionPause": False, "actionSave": False,
-              "actionShowLibrary": False, "actionPlay": True,
-              "actionRemove": True, "actionShowRecorder": True, })
-
-    IMAGES = {"record": "iconmonstr-microphone-3-icon-256.png",
-              "pause": "pause-2-icon-256.png",
-              "window": "iconmonstr-script-8-icon-256.png"}
+    IMAGES = {"play": "iconmonstr-video-play-icon-256.png",
+              "stop": "iconmonstr-stop-2-icon-256.png",
+              "record": "iconmonstr-microphone-3-icon-256.png",
+              "add": "iconmonstr-plus-4-icon-256.png"}
 
     def __init__(self, *args, **kwds):
         super(Layout, self).__init__(*args, **kwds)
@@ -40,176 +29,125 @@ class Layout(QtGui.QMainWindow):
         self.ui.setupUi(self)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
         # args
-        self.current_macro = None
         self.IMAGES = dict([(k, os.path.join(os.path.dirname(__file__), "images", v))
                            for k, v in self.IMAGES.iteritems()])
         self.ICONS = dict([(k, QtGui.QIcon(v))
                           for k, v in self.IMAGES.iteritems()])
-        # update window title
+        self._state = 2  # 0: record, 1: stop, 2:play
+        # connect signals
+        self.ui.filter.textChanged.connect(self.filter_changed)
+        self.ui.filter.returnPressed.connect(self.filter_entered)
+        self.ui.action.clicked.connect(self.action_clicked)
+        self.ui.macros.itemSelectionChanged.connect(self.macros_changed)
+        # init ui
         try:
             _version = pkg_resources.get_distribution("mauto").version
             self.setWindowTitle("mauto v%s" % _version)
         except:
             pass
-        self.setWindowIcon(self.ICONS.get("window"))
-        # connect signals
-        self.ui.filter.textChanged.connect(self.filter_list)
-        self.ui.button.clicked.connect(self.btn_clicked)
-        # menues signals
-        self.ui.actionRecord.triggered.connect(self.record)
-        self.ui.actionPause.triggered.connect(self.pause)
-        self.ui.actionSave.triggered.connect(self.save)
-        self.ui.actionRemove.triggered.connect(self.remove_macro)
-        self.ui.actionShowRecorder.triggered.connect(
-            lambda x=0: self.set_mode(x))
-        self.ui.actionShowLibrary.triggered.connect(
-            lambda x=1: self.set_mode(x))
-        self.ui.button.setIcon(self.ICONS.get("record"))
-        # init mode
-        self.set_mode(0)
+        self.list_macros()
+        self.filter_changed("")
 
-    def show_macros(self):
-        self.ui.list.clear()
-        self.ui.list.addItems(library.keys())
+    @property
+    def state(self):
+        if hasattr(self, "_state"):
+            return self._state
 
-    def show_actions(self):
-        self.ui.list.clear()
-        if self.current_macro:
-            _items = [unicode(x) for x in self.current_macro.actions]
-            self.ui.list.addItems(_items)
+    @state.setter
+    def state(self, value):
+        self._state = value
+        _icon = self.ICONS.get(("add", "stop", "play")[self._state])
+        self.ui.action.setIcon(_icon)
+        css = "#MainWindow{border: 2px solid;border-color:rgb(255,0,0);}" if self._state == 1 else ""
+        self.setStyleSheet(css)
 
-    def set_mode(self, index):
-        # index == 0: Recorder, index==1: Library
-        self._mode = index
-        for k, v in self.MODES[index].iteritems():
-            getattr(self.ui, k).setVisible(v)
-        if self._mode:
-            self.close_recorder()
-            self.show_macros()
-        else:
-            self.show_actions()
+    def list_macros(self):
+        self.ui.macros.setRowCount(len(library))
+        for i, m in enumerate(library.values()):
+            item = QtGui.QTableWidgetItem()
+            item.setText(m.name)
+            item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+            self.ui.macros.setItem(i, 0, item)
 
-    def close_recorder(self):
-        if hasattr(self, "_recording"):
-            del self._recording
-        if self.current_macro:
-            library.remove_macro(self.current_macro.name)
-            self.current_macro = None
-
-    def filter_list(self, text):
-        for i in range(self.ui.list.count()):
-            item = self.ui.list.item(i)
-            item.setHidden(text not in item.text())
-
-    def remove_macro(self):
-        item = self.ui.list.currentItem()
-        if not item:
+    def list_inputs(self):
+        curr_item = self.ui.macros.currentItem()
+        curr_macro = library.get(curr_item.text()) if curr_item else None
+        if not curr_macro:
             return
-        i = self.ui.list.row(item)
-        n = item.text()
-        self.ui.list.takeItem(i)
-        library.remove_macro(n)
-
-    def btn_clicked(self):
-        if not hasattr(self, "_recording"):
-            self.new()
-        (self.record, self.pause)[int(not self._recording)]()
-
-    def pause(self):
-        if not hasattr(self, "_recording"):
-            return
-        self.ui.button.setIcon(self.ICONS.get("record"))
-        self.ui.list.setStyleSheet("")
-        self._recording = not self._recording
-        # macro stuff
-        self.current_macro.pause()
-        library.save_macro(self.current_macro.name)
-        self.show_actions()
-
-    def save(self):
-        if self.current_macro:
-            if Inputs.save(self.current_macro, parent=self):
-                self.set_mode(1)
-
-    def record(self):
-        if not hasattr(self, "_recording"):
-            self.new()
-        self.ui.button.setIcon(self.ICONS.get("pause"))
-        stylesheet = "border: 2px solid; border-color: rgb(255, 0, 0);"
-        self.ui.list.setStyleSheet(stylesheet)
-        self._recording = not self._recording
-        # macro stuff
-        self.current_macro.record()
-
-    def new(self):
-        self._recording = True
-        # start a new macro
-        self.current_macro = library.new_macro(str(int(time.time())))
-
-    def closeEvent(self, event):
-        self.close_recorder()
-        super(Layout, self).closeEvent(event)
-
-
-class Inputs(QtGui.QDialog):
-
-    def __init__(self, macro, *args, **kwds):
-        super(Inputs, self).__init__(*args, **kwds)
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
-        # args
-        self.macro = macro
-        # signals
-        self.ui.name.textChanged.connect(self.validate_name)
-        self.validate_name(self.ui.name.text())
-        # fill inputs
-        self.ui.inputs.setRowCount(len(self.macro.inputs))
-        self.ui.inputs.setColumnCount(2)  # key, value
-        for row, items in enumerate(self.macro.inputs.iteritems()):
+        self.ui.inputs.setRowCount(len(curr_macro.inputs))
+        for row, items in enumerate(curr_macro.inputs.iteritems()):
             for column, value in enumerate(items):
                 item = QtGui.QTableWidgetItem()
-                item.setText(str(value))
+                item.setText(value)
+                if column == 0:
+                    item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
                 self.ui.inputs.setItem(row, column, item)
 
-    @classmethod
-    def play(cls, macro, *args, **kwds):
-        d = cls(macro, *args, **kwds)
-        d.setWindowTitle("Inputs editor")
-        d.ui.name.setVisible(False)
-        result = d.exec_()
-        if result:
-            macro.play(d.refs)
-        del d
-        return result
+    def filter_changed(self, text):
+        _match = False
+        for i in range(self.ui.macros.rowCount()):
+            item = self.ui.macros.item(i, 0)
+            if item:
+                if text == item.text():
+                    _match = True
+                self.ui.macros.setRowHidden(i, text not in item.text())
+        self.state = 2 if _match else 0
 
-    @classmethod
-    def save(cls, macro, *args, **kwds):
-        d = cls(macro, *args, **kwds)
-        d.setWindowTitle("Save macro as...")
-        d.ui.name.setFocus()
-        result = d.exec_()
-        if result:
-            name = d.ui.name.text()
-            print macro.actions
-            m = library.new_macro(name)
-            m.deserialize(macro.serialize())
-            m.filepath = None
-            library.save_macro(name)
-        del d  # release macro instance
-        return result
+    def filter_entered(self):
+        name = self.ui.filter.text()
+        if not len(name):
+            return
+        for i in range(self.ui.macros.rowCount()):
+            item = self.ui.macros.item(i, 0)
+            if item and item.text() == name:
+                self.macros_changed(item)
+                return
+        self.action_clicked()
 
-    def validate_name(self, text):
-        stylesheet = "border: 1px solid; border-color: rgb(255, 0, 0);"
-        is_valid = library.get(text) is None and len(text)
-        self.ui.name.setStyleSheet("" if is_valid else stylesheet)
-        return is_valid
+    def macros_changed(self):
+        _item = self.ui.macros.currentItem()
+        m = library.get(_item.text())
+        if not m:
+            return
+        self.list_inputs()
+        self.state = 2
 
-    def accept(self):
-        if self.ui.name.isVisible():
-            if self.validate_name(self.ui.name.text()):
-                super(Inputs, self).accept()
-        else:
-            super(Inputs, self).accept()
+    def remove_macro(self):
+        item = self.ui.macros.currentItem()
+        if not item:
+            return
+        for i in range(self.ui.macros.rowCount()):
+            curr_item = self.ui.macros.item(i, 0)
+            if curr_item.text() == item.text():
+                self.ui.macros.removeRow(i)
+                library.remove_macro(item.text())
+
+    def action_clicked(self):
+        if len(self.ui.filter.text()):
+            (self.record, self.stop, self.play)[self.state]()
+
+    def stop(self):
+        self.state = 2
+        m = library.get(self.ui.filter.text())
+        m.pause()
+
+    def record(self):
+        name = self.ui.filter.text()
+        # update ui
+        self.state = 1
+        item = QtGui.QTableWidgetItem(name)
+        item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+        self.ui.macros.insertRow(0)
+        self.ui.macros.setItem(0, 0, item)
+        self.ui.macros.setCurrentItem(item)
+        # macro stuff
+        if library.get(name):
+            library.remove_macro(name)
+        m = library.new_macro(name)
+        m.record()
+
+    def play(self):
+        pass
 
 
 def get_parent():
